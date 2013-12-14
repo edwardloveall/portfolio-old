324df96f231cceda68008e21300b32bafbaa5af9
Edward Loveall <edward@edwardloveall.com>
2013-12-14 16:24:03 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'debugger', group: [:development, :test]
+
+gem 'thin'
+gem 'rspec-rails', group: [:development, :test]
+gem 'quiet_assets', group: :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '0b463621b808bccd106d2c6ef71887519bcf99f260c3ac8cbad6b9bc7a8308d55b651f7d0361c68dadd4718cedf5d64245fd87f88cb325cba2e3ea1f10a4979b'
